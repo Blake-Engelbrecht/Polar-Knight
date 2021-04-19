63292a35fe9e769c515c7377c12cd746b94be81a
Blake Engelbrecht <bengelbrecht95@gmail.com>
2021-04-19 18:59:27 -0500

    added asset files

--- player/Player.hx
+++ player/Player.hx
@@ -3,5 +3,9 @@ package player;
 import flixel.FlxSprite;
 
 class Player extends FlxSprite {
-    
+    public function new(xPos:Int = 500, yPos:Int = 200) {
+        super(xPos, yPos);
+        loadGraphic("assets/images/player/Idle.png", true, 64, 64);
+        
+    }
 }
--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -1,11 +1,15 @@
 package states;
 
+import player.Player;
 import flixel.FlxState;
 
 class PlayState extends FlxState {
 
 	override public function create() {		
 		super.create();
+		bgColor = 0xff8fbcbb;
+		final player = new Player();
+		add(player);
 	}
 
 	override public function update(elapsed:Float) {
