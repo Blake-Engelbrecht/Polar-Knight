05faf1392b188c0c8cc4d1f764bf94cb7c3980c3
Blake Engelbrecht <bengelbrecht95@gmail.com>
2021-04-19 15:00:10 -0500

    added main menu and play button

---
+++ source/MenuState.hx
@@ -0,0 +1,27 @@
+package;
+
+import flixel.FlxG;
+import flixel.ui.FlxButton;
+import flixel.FlxState;
+
+class MenuState extends FlxState
+{
+    var PlayButton:FlxButton;
+
+	override public function create() {		
+        PlayButton = new FlxButton(0, 0, "Play", clickPlay);
+        add(PlayButton);
+        PlayButton.screenCenter();
+
+        super.create();
+	}
+
+    function clickPlay() {
+        FlxG.switchState(new PlayState());
+        
+    }
+
+	override public function update(elapsed:Float) {
+		super.update(elapsed);
+	}
+}
