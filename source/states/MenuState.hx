95df0a3278af6e76402013cb2a32627b4f2b7dd5
Blake Engelbrecht <bengelbrecht95@gmail.com>
2021-04-21 00:02:07 -0500

    added title to main menu

--- source/states/MenuState.hx
+++ source/states/MenuState.hx
@@ -1,28 +1,33 @@
 package states;
 
 import flixel.FlxG;
-import flixel.ui.FlxButton;
 import flixel.FlxState;
-
+import flixel.text.FlxText;
+import flixel.ui.FlxButton;
 
 class MenuState extends FlxState
 {
-    var PlayButton:FlxButton;
+	var PlayButton:FlxButton;
 
-	override public function create() {		
-        PlayButton = new FlxButton(0, 0, "Play", clickPlay);
-        add(PlayButton);
-        PlayButton.screenCenter();
+	override public function create()
+	{
+		PlayButton = new FlxButton(0, 0, "Play", clickPlay);
+		add(PlayButton);
+		PlayButton.screenCenter();
 
-        super.create();
+		super.create();
+
+		var text = new flixel.text.FlxText(80, 10, 0, "Polar Knight", 20);
+		add(text);
 	}
 
-    function clickPlay() {
-        FlxG.switchState(new PlayState());
-        
-    }
+	function clickPlay()
+	{
+		FlxG.switchState(new PlayState());
+	}
 
-	override public function update(elapsed:Float) {
+	override public function update(elapsed:Float)
+	{
 		super.update(elapsed);
 	}
 }
