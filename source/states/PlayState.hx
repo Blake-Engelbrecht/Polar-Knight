a5965bf1d33884f8fc9a8ca861935a44bb8135a7
Blake Engelbrecht <bengelbrecht95@gmail.com>
2021-04-20 23:44:16 -0500

    more in-depth level creation. added background and foreground assets. camera is more zoomed in and follows player.

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -15,23 +15,42 @@ class PlayState extends FlxState
 
 	var map:FlxOgmo3Loader;
 	var ground:FlxTilemap;
+	var background:FlxTilemap;
+	var foreground:FlxTilemap;
+	var snow:FlxTilemap;
+	var backgroundTrees:FlxTilemap;
 
 	override public function create()
 	{
-		super.create();
+		bgColor = 0xFF2E3440;
 
 		map = new FlxOgmo3Loader("assets/data/polar-knight-testLevel.ogmo", "assets/data/testLevel.json");
 		ground = map.loadTilemap("assets/images/pk-ground-tile.png", "ground");
+		background = map.loadTilemap("assets/images/pk-background-tile.png", "background");
+		foreground = map.loadTilemap("assets/images/pk-foreground-tile.png", "foreground");
+		snow = map.loadTilemap("assets/images/pk-snow-tile.png", "snow");
+		backgroundTrees = map.loadTilemap("assets/images/pk-tree-tile.png", "backgroundTrees");
+
 		ground.follow();
-		ground.setTileProperties(1, FlxObject.ANY);
+		ground.setTileProperties(1, FlxObject.NONE);
+		ground.setTileProperties(2, FlxObject.ANY);
+		ground.setTileProperties(3, FlxObject.ANY);
+
+		add(background);
+		// add(snow);
+		add(backgroundTrees);
 		add(ground);
 
 		player = new Player();
 		player.setSize(30, 45);
 		player.offset.set(78, 70);
-		map.loadEntities(placeEntities, "entities");
+		map.loadEntities(placeEntities, "entities"); // player spawn point position
 
 		add(player);
+		add(foreground);
+		FlxG.camera.follow(player);
+
+		super.create();
 	}
 
 	function placeEntities(entity:EntityData)
