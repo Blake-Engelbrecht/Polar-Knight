1ee9b82f475e7d212e582d139e202a84cf553648
Blake Engelbrecht <bengelbrecht95@gmail.com>
2021-05-05 17:47:40 -0500

    moved files to doc folder for GitHub demo

---
+++ docs/assets/data/background1.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="background1" tilewidth="400" tileheight="300" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="400" height="300" source="../../../../../Photoshop/Game Assets/Polar-Knight/Winter Forest/Background 2.png"/>
+ </tile>
+</tileset>
